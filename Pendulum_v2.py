cae4195df016b76599a698e6d40042d876522c7a
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-04-12 07:45:47 +0800

    0412 pendulum v2 update

--- Pendulum_v2.py
+++ Pendulum_v2.py
@@ -34,22 +34,34 @@ class Pendulum:
         self.len_rod = 0.35
         self.len_wheel = 0.5
         self.mass_rod = 20
-        self.rad_out = 0.055
-        self.rad_in = 0.032
-        self.t = 0.02
+        self.rad_out = 0.05
+        self.rad_in = 0.045
+        self.t = 0.01
         self.rho = 7870
         self.mass_wheel = (self.rad_out**2-self.rad_in**2)*pi*self.t*self.rho
-        self.momentum_rod = self.mass_rod*(2*self.len_rod)**2/33
+        #self.momentum_rod = self.mass_rod*(2*self.len_rod)**2/33
+        self.momentum_rod=(2*self.len_rod)*self.mass_rod*(2*self.len_rod)**2/12+(1-(2*self.len_rod))*self.mass_rod*(1-(2*self.len_rod))**2/12
         self.momentum_wheel = self.mass_wheel*(self.rad_out**2+self.rad_in**2)/2
         self.dt = 0.001
         self.gravity = 9.81
+        self.max_q1dot = 1 #initial q1_dot default 1? to be verified
         self.wheel_max_speed = 20
-        self.max_torque = 10
+        self.max_torque = 15
         self.torque = 0
-        self.voltage = 0
 
-        high = np.array([pi, 0.5, self.wheel_max_speed], dtype=np.float32)
-        self.action_space = spaces.Box(low=-self.max_torque, high=self.max_torque, shape=(1,), dtype=np.float32)
+        l1 = self.len_rod
+        l2 = self.len_wheel
+        m1 = self.mass_rod
+        m2 = self.mass_wheel
+        I1 = self.momentum_rod
+        I2 = self.momentum_wheel
+
+        self.Ip = m2*l2**2+I1+I2 + 2*l1*m1*l1**2+(1-2*l1)*m1*(0.5*(1-2*l1)+2*l1)**2
+        self.mbarg = (m1*l1+m2*l2)*self.gravity
+
+        high = np.array([pi/4, self.max_q1dot, self.wheel_max_speed], dtype=np.float32)
+        #self.action_space = spaces.Box(low=-self.max_torque, high=self.max_torque, shape=(1,), dtype=np.float32)
+        self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(low=-high, high=high, dtype=np.float32)
 
         #print(self.mass_wheel)
@@ -74,51 +86,23 @@ class Pendulum:
 
     def reset(self, saved, seed):
         self.np_random = np.random.seed(seed)
-        high = np.array([pi, 0.5, self.wheel_max_speed])
-        self.state = self.np_random.uniform(low=-high, high=high)
-        self.last_u = None
-        if not return_info:
-            return np.array([self.theta_rod, self.theta_rod_dot, self.theta_wheel_dot], dtype=np.float32)
 
-        roll_range = 3 #in degree
-        self.ang = roll_range
-        #reset_max_speed = 3
+        reset_angle_deg = 10
+        reset_angle_rad = reset_angle_deg*pi/180
 
         if saved == None:
-            self.theta_rod = np.random.uniform(low=-roll_range * pi / 180, high=roll_range * pi / 180)
-        elif saved != None:
-            self.theta_rod = roll_range * 1 * pi / 180
-        #print(self.theta_rod*180/pi)
-
-        #self.theta_rod = np.random.uniform(low=-roll_range*pi/180, high=roll_range*pi/180)
-        #self.theta_rod = np.random.uniform(low=0, high=roll_range*pi/180)
-        #self.theta_rod = roll_range*0.3*pi/180
-        #print(self.theta_rod)
-
-        #print("\n",self.theta_rod)
-        #self.theta_rod = (np.random.random()*2-1)*roll_range*pi/180
-        #self.theta_wheel = 0
-        self.theta_rod_dot = 0
-        #self.theta_rod_dot = (np.random.random() * 2 - 1) * reset_max_speed
-        self.theta_wheel_dot = 0
-
-        '''
-        ##############################
-        # reset wheel velocity
-        wheel_vel = 20
-        if saved == None:
-            self.theta_wheel_dot = np.random.uniform(low=-wheel_vel, high=wheel_vel)
+            reset_high = np.array([reset_angle_rad, self.max_q1dot, self.wheel_max_speed])
+            self.state = self.np_random.uniform(low=-reset_high, high=reset_high)
         else:
-            self.theta_wheel_dot = wheel_vel
+            self.state = np.array([reset_angle_rad, 0, 0], dtype=np.float32)
+            # self.state = np.array([0, self.max_q1dot, 0],dtype=np.float32)
 
-        self.theta_rod = 0
-        self.theta_rod_dot = 0
+        self.last_u = None
 
-        ##############################
-        '''
-
-        state = np.array([self.theta_rod, self.theta_rod_dot, self.theta_wheel_dot], dtype=np.float32)
-        return state
+        if not return_info:
+            return self.state
+        else:
+            return self.state, {}
 
 
     def render(self, eval_run):
@@ -154,61 +138,45 @@ class Pendulum:
 
 
     def step(self, action):
-        q1 = self.theta_rod
+        q1, q1_dot, q2_dot = self.state
+
+        #q1 = self.theta_rod
         q2 = self.theta_wheel
-        q1_dot = self.theta_rod_dot
-        q2_dot = self.theta_wheel_dot
-        l1 = self.len_rod
-        l2 = self.len_wheel
-        m1 = self.mass_rod
-        m2 = self.mass_wheel
-        I1 = self.momentum_rod
+        #q1_dot = self.theta_rod_dot
+        #q2_dot = self.theta_wheel_dot
+        #l1 = self.len_rod
+        #l2 = self.len_wheel
+        #m1 = self.mass_rod
+        #m2 = self.mass_wheel
+        #I1 = self.momentum_rod
         I2 = self.momentum_wheel
         dt = self.dt
-        g = self.gravity
-        #gear_ratio = 25
-        #kt = 0.0229
-        #ke = 0.0229
-        #R = 0.71
-        #action_scale = 12
-        #gear_ratio = 7
-        #kt = 0.55
-        #ke = 0.34
-        #R = 0.38
-        #action_scale = 48
+        #g = self.gravity
+        #action_scale = self.max_torque
 
-        action_scale = self.max_torque
+        #torque = action * action_scale
+        #torque = np.clip(torque, -self.max_torque, self.max_torque)
 
-        torque = action * action_scale
-        #voltage = action * action_scale
-        #torque = gear_ratio*kt/R*(voltage-ke*gear_ratio*q2_dot)
-        torque = np.clip(torque, -self.max_torque, self.max_torque)
-        #print("v",voltage)
-        # print("T",torque)
-        #print("q2dot",q2_dot)
-        #print("q2", q2)
+        if action == 0:
+            torque = 0
+        elif action == 1:
+            torque = -self.max_torque
+        elif action == 2:
+            torque = self.max_torque
+        else:
+            torque = 1000000
 
-        Ip = m1*l1**2+m2*l2**2+I1+I2
-        a = (m1*l1+m2*l2)*g*sin(angle_normalize(q1))
+        #Ip = m2*l2**2+I1+I2 + 2*l1*m1*l1**2+(1-2*l1)*m1*(0.5*(1-2*l1)+2*l1)**2
+        #a = (m1*l1+m2*l2)*g*sin(angle_normalize(q1))
+        Ip = self.Ip
+        a = self.mbarg*sin(angle_normalize(q1))
 
         newq1_dot = q1_dot + ((a-torque)/(Ip-I2))*dt
-        #print("rod ang_vel",newq1_dot)
-        #newq1_dot = np.clip(newq1_dot, -self.wheel_max_speed, self.wheel_max_speed)
-        #print("rod ang_vel",newq1_dot)
         newq1 = angle_normalize(angle_normalize(q1) + newq1_dot * dt)
-        #print("rod angle",newq1)
 
         newq2_dot = q2_dot + ((torque*Ip-a*I2)/I2/(Ip-I2))*dt
         newq2_dot = np.clip(newq2_dot, -self.wheel_max_speed, self.wheel_max_speed)
-        #print("wheel ang_vel",newq2_dot)
         newq2 = angle_normalize(angle_normalize(q2) + newq2_dot * dt)
-        #print("wheel angle",newq2)
-
-        #print("torque",torque)
-        #print("\n")
-        #print([torque, newq1[0], newq2[0], newq1_dot[0], newq2_dot[0]])
-
-        # print("newq1",newq1)
 
         state = np.array([newq1[0], newq1_dot[0], newq2_dot[0]], dtype=np.float32)
 
@@ -217,15 +185,6 @@ class Pendulum:
         self.theta_rod_dot = newq1_dot
         self.theta_wheel_dot = newq2_dot
         self.torque = torque
-        #self.voltage = voltage
-
-        #print(newq2_dot)
-        '''
-        if angle_normalize(q1) < self.ang*pi/180/10:
-            costs = 1000 * angle_normalize(q1)**2 + 0.1 * q1_dot**2 + 0.001 * voltage**2
-        elif angle_normalize(q1) >= self.ang*pi/180/10:
-            costs = 1000 * angle_normalize(q1) ** 2 + 0.001*48**2
-        '''
 
         #costs = 1000 * angle_normalize(q1) ** 2 + 0.1 * q1_dot ** 2 + 0.001 * torque ** 2
         # costs = 1000 * angle_normalize(q1) ** 2 + 0.1 * q1_dot ** 2 + 0.001 * torque ** 2 + 0.00001 * q2_dot**2
@@ -239,58 +198,6 @@ class Pendulum:
         #    costs -= 1000
 
         return state, -costs, False, {}
-
-    '''
-    def step_q2dot(self, action):
-        q1 = self.theta_rod
-        q2 = self.theta_wheel
-        q1_dot = self.theta_rod_dot
-        q2_dot = self.theta_wheel_dot
-        l1 = self.len_rod
-        l2 = self.len_wheel
-        m1 = self.mass_rod
-        m2 = self.mass_wheel
-        I1 = self.momentum_rod
-        I2 = self.momentum_wheel
-        dt = self.dt
-        g = self.gravity
-
-        action_scale = self.max_torque
-
-        torque = action * action_scale
-        torque = np.clip(torque, -self.max_torque, self.max_torque)
-
-        Ip = m1*l1**2+m2*l2**2+I1+I2
-        a = (m1*l1+m2*l2)*g*sin(angle_normalize(q1))
-
-        newq1_dot = q1_dot + ((a-torque)/(Ip-I2))*dt
-        newq1 = angle_normalize(angle_normalize(q1) + newq1_dot * dt)
-
-        newq2_dot = q2_dot + ((torque*Ip-a*I2)/I2/(Ip-I2))*dt
-        newq2_dot = np.clip(newq2_dot, -self.wheel_max_speed, self.wheel_max_speed)
-        newq2 = angle_normalize(angle_normalize(q2) + newq2_dot * dt)
-
-        state = np.array([newq1[0], newq1_dot[0], newq2_dot[0]], dtype=np.float32)
-
-        self.theta_rod = newq1
-        self.theta_wheel = newq2
-        self.theta_rod_dot = newq1_dot
-        self.theta_wheel_dot = newq2_dot
-        self.torque = torque
-
-        #costs = 1000 * angle_normalize(q1) ** 2 + 0.1 * q1_dot ** 2 + 0.001 * torque ** 2
-        #costs = 1000 * angle_normalize(q1) ** 2 + 0.1 * q1_dot ** 2 + 0.001 * torque ** 2 + 0.00001 * q2_dot**2
-        # costs = 100 * angle_normalize(q1) ** 2 + 1 * q1_dot ** 2
-        costs = 0.0001 * q2_dot ** 2
-
-        
-        #if abs(angle_normalize(q1)) < 0.002 and abs(q1_dot) < 0.002 and abs(q2_dot) < 0.1 :
-        #    costs -= 100
-        #elif abs(angle_normalize(q1)) < 0.001 and abs(q1_dot) < 0.001 and abs(q2_dot) < 0.01 :
-        #    costs -= 1000
-        
-        return state, -costs, False, {}
-    '''
 
     def close(self):
         pygame.display.quit()
